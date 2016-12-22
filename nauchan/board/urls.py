9415e95a778c1045244c0a08e17ef66c16a739a2
Yury Mironov <yuriymironov96@gmail.com>
2016-12-22 21:12:07 +0200

    Add basic api

--- nauchan/board/urls.py
+++ nauchan/board/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
-from views import ThreadListView
+from views import ThreadListView, ThreadView
 
 
 urlpatterns = [
-    url(r'^(?P<string>[\w]{1, 3})/$', ThreadListView.as_view(), name='thread-list')
+    url(r'^(?P<board>[a-z]{1,3})/$',
+                                ThreadListView.as_view(), name='thread-list'),
+    url(r'^(?P<board>[a-z]{1,3})/(?P<thread_id>[1-9]{1,3})/$',
+                                    ThreadView.as_view(), name='thread-view'),
 ]
